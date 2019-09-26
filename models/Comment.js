e70841d8eba06ee15b25e59cd4c46ae5adecfb3a
Spencer Summerville <me@grenuttag.com>
2019-09-25 20:04:00 -0400

    Initial comment support

---
+++ models/Comment.js
@@ -0,0 +1,15 @@
+const knex = require("../database/db.js");
+
+module.exports = { getByID, create };
+
+function getByID(id) {
+  return knex("comments")
+    .where({ id })
+    .first();
+}
+
+function create(comment) {
+  return knex("comments")
+    .insert({ comment })
+    .then(id => getByID(id)[0]);
+}
---
+++ routes/comments.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const { checkSchema } = require("express-validator");
+
+const Comment = require("../models/Comment.js");
+const authenticate = require("../middleware/authenticate.js");
+const checkValidation = require("../middleware/validation.js");
+
+router.post(
+  "/",
+  authenticate,
+  checkSchema({
+    request_id: {
+      in: ["body"],
+      isEmpty: false,
+      isInt: true
+    },
+    body: {
+      in: ["body"],
+      isEmpty: false
+    }
+  }),
+  checkValidation,
+  (req, res) => {
+    Comment.create({ user_id: req.token.sub, ...req.body }).then(comment =>
+      res.status(201).json(comment)
+    );
+  }
+);
